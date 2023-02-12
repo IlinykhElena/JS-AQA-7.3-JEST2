014fa95944cb4b096c27ef2046ae75d51be79a2e
ElenaIlinykh <bengaliki22@gmail.com>
2023-02-12 09:01:36 +0700

    Files created

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,35 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+
+  it("Books names should be sorted in descending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Гарри Поттер",
+      "Волшебник изумрудного города",
+      "Властелин Колец",
+    ]);
+  });
+
+  it("Books names should be not sorted", () => {
+    expect(sorting.sortByName(["Гарри Поттер"])).toEqual(["Гарри Поттер"]);
+  });
+});
