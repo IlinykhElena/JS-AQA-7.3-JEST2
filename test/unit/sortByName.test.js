66412aaebbe16621bea73a1c89b555b5f8656f10
ElenaIlinykh <bengaliki22@gmail.com>
2023-03-31 08:52:15 +0700

    right coverage got

--- test/unit/sortByName.test.js
+++ test/unit/sortByName.test.js
@@ -15,25 +15,17 @@ describe("Books names test suit", () => {
     ]);
   });
 
-  it("Books names should be sorted in descending order", () => {
+  it("Books names should be not sorted", () => {
     expect(
       sorting.sortByName([
-        "Гарри Поттер",
         "Властелин Колец",
-        "Волшебник изумрудного города",
+        "ВЛАСТЕЛИН КОЛЕЦ",
+        "вЛаСтЕлИн кОлЕц",
       ])
-    ).toEqual([
-      "Гарри Поттер",
-      "Волшебник изумрудного города",
-      "Властелин Колец",
-    ]);
+    ).toEqual(["Властелин Колец", "ВЛАСТЕЛИН КОЛЕЦ", "вЛаСтЕлИн кОлЕц"]);
   });
 
-  it("Books names should be not sorted", () => {
-    expect(sorting.sortByName(["Гарри Поттер"])).toEqual(["Гарри Поттер"]);
-  });
-
-  it("Books names should throw exception", () => {
-    expect(sorting.sortByName([])).toThrow(TypeError);
+  it("There are no book", () => {
+    expect(sorting.sortByName([])).toEqual([]);
   });
 });
